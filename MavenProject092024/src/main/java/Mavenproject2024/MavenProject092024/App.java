705acf6e78a943539f20ebe456ba3b2100a965f1
Aadhya sri <Aadhya sri@LAPTOP-G38FHJL3>
2024-11-29 11:04:45 +0530

    Commit the MavenProject092024

---
+++ MavenProject092024/src/main/java/Mavenproject2024/MavenProject092024/App.java
@@ -0,0 +1,13 @@
+package Mavenproject2024.MavenProject092024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
